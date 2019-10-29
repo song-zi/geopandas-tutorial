5c0012d5747b03233f51d215cd7f74f59823da66
song-zi <song-zi@users.noreply.github.com>
2019-10-29 21:07:28 +0800

    完成第一部分代码10.29

--- peactice1.py
+++ peactice1.py
@@ -5,61 +5,82 @@ import geopandas
 import matplotlib.pyplot as plt
 from shapely.geometry import Point, Polygon, LineString
 
+# 英文教程 https://geopandas.readthedocs.io/en/latest/install.html
+# 中文教程 https://www.bbsmax.com/A/Vx5M9KyL5N/
 
 countries = geopandas.read_file(r"geopandas-tutorial-song\data\ne_110m_admin_0_countries\ne_110m_admin_0_countries.shp")
 cities = geopandas.read_file(r"geopandas-tutorial-song\data\ne_110m_populated_places")
 rivers = geopandas.read_file(r"geopandas-tutorial-song\data\ne_50m_rivers_lake_centerlines")
 
 
-def print_head_date(__data):
+def print_head_date():
     '''显示前几行数据'''
-    head = __data.head()
+    __countries = countries
+    head = __countries.head()
     print(head)
 
 
-def print_all_graph(__data):
+def print_all_graph():
     '''打印shp'''
-    __data.plot()
+    __countries = countries
+    __countries.plot()
     # countries.show() the code is wrong
     # 难死我了，终于解决无法show（）的问题了，好多地方给的代码都是错的。
  
-    return __data
+    return __countries
 
 
-def print_area(__data):
+def print_area():
     '''打印国家、州、面积'''
-    __area = __data.geometry.area
-    __continent = __data['continent']
-    name = __data['name']
-    for i in zip(name, __continent, __area):
+    __countries = countries
+    area = __countries.geometry.area
+    continent = __countries['continent']
+    name = __countries['name']
+    for i in zip(name, continent, area):
         print(i)
 
 
-def print_africa():
+def print_continent(continent):
     '''打印一个部分'''
-    __africa = countries[countries['continent'] == 'Africa']
-    __africa.plot()
+    __countries = countries
+    continent = __countries[__countries['continent'] == continent ]
+    continent.plot()
     plt.show()
 
-def mean_all(__data):
+# print(countries.head())
+# print_africa('Asia')
+
+
+def mean_all():
     '''mean()函数功能：求取均值'''
-    __mean = __data['pop_est'].mean()
-    print('mean is', __mean)
-    return __mean
+    __countries = countries
+    mean = __countries['pop_est'].mean()
+    print('mean is', mean)
+    return mean
 
-def get_centroid(__data):
+# mean_all()
+
+
+def get_centroid(data):
     '''求质心'''
+    __countries = data
     # 新增一列，每个国家的中心点
-    __data['geometry'] = __data.centroid
+    __countries['geometry'] = __countries.centroid
     # 将新增列设置为几何列
-    __data = __data.set_geometry('geometry') 
-    # __data.plot()
+    __countries = __countries.set_geometry('geometry') 
+    # __countries.plot()
     # plt.show()
-    return __data
+    return __countries
 
-def data_to_shp(__data):
+# c = get_centroid(countries)
+# c.plot()
+# plt.show()
+
+
+def data_to_shp(data):
+    __countries = data
     '''把GeoDataFrame数据导出到shp文件'''
-    __data.to_file(driver='ESRI Shapefile',filename='world_out.shp')
+    __countries.to_file(driver='ESRI Shapefile',filename='countries_out.shp')
 
 
 def area_and_distance():
@@ -82,6 +103,51 @@ def show_together():
     ax.set(xlim=(-20, 60), ylim=(-40, 40))
     return ax
 
-ax = show_together()
 
-plt.show()
+
+def NDD():
+    '''转换GeoSeries中的几何图形到不同的坐标参考系统。
+    当前GeoSeries的crs属性必须被设置。
+    crs属性需要被指定以用于输出，或是用字典形式或是用EPSG编码方式。'''
+    __countries = countries
+    __countries = __countries[(__countries['name'] != "Antarctica")]
+    countries_mercator = __countries.to_crs(epsg=3395)
+
+    countries_mercator.plot()
+
+    plt.show()
+
+
+# Note on fiona
+
+def fiona():
+    # 'https://blog.csdn.net/theonegis/article/details/80607262
+    import fiona
+    from shapely.geometry import shape
+
+    with fiona.Env():
+        with fiona.open(r"geopandas-tutorial-song\data\ne_110m_admin_0_countries\ne_110m_admin_0_countries.shp") as collection:
+            for feature in collection:
+                # ... do something with geometry
+                geom = shape(feature['geometry'])
+                # ... do something with properties
+                print(feature['properties']['name'])
+
+
+def new_gdf():
+    '''手动创建一个geodataframe类型数据'''
+    df = pd.DataFrame(
+        {'City': ['Buenos Aires', 'Brasilia', 'Santiago', 'Bogota', 'Caracas'],
+         'Country': ['Argentina', 'Brazil', 'Chile', 'Colombia', 'Venezuela'],
+         'Latitude': [-34.58, -15.78, -33.45, 4.60, 10.48],
+         'Longitude': [-58.66, -47.91, -70.66, -74.08, -66.86]})
+    # 把经纬度打包成一列（x, y）
+    df['Coordinates']  = list(zip(df.Longitude, df.Latitude))
+    # （x, y）把转换成点
+    df['Coordinates'] = df['Coordinates'].apply(Point)
+    #  把'Coordinates'转换成geometry(几何形状)
+    gdf = geopandas.GeoDataFrame(df, geometry='Coordinates')
+    ax = countries[countries.continent == 'South America'].plot(
+        color='white', edgecolor='black')
+    gdf.plot(ax=ax, color='red')
+    plt.show()
